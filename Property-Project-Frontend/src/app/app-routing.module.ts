c3b0efb5048f0e6fb155393328b364909725a27d
Robert Wood <L.robertwood@gmail.com>
2022-06-14 19:05:55 -0400

    Setting up app.component and routers

--- Property-Project-Frontend/src/app/app-routing.module.ts
+++ Property-Project-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
