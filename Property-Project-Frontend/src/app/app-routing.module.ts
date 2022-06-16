5bef34513ffea24496918406a3a0aed133e81636
Robert Wood <L.robertwood@gmail.com>
2022-06-15 21:54:20 -0400

    styling and page layout for about and booking

--- Property-Project-Frontend/src/app/app-routing.module.ts
+++ Property-Project-Frontend/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { BookingComponent } from './booking/booking.component';
 
 const routes: Routes = [
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'booking', component: BookingComponent}
 ];
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Property-Project-Frontend/src/app/app.module.ts
+++ Property-Project-Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
