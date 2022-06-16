1fac356310571665cf23841703f3640d82a012bf
jtharg <j.t.hargenrader@email.msmary.edu>
2022-06-15 22:03:59 -0400

    Booking Page created

--- Property-Project-Frontend/src/app/app-routing.module.ts
+++ Property-Project-Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { BookingComponent } from './booking/booking.component';
 
 const routes: Routes = [
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'booking', component: BookingComponent}
 ];
 
 
